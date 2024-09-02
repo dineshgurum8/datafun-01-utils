f939dce83f141b53122f0e15b1313fa877e60ffc
dineshgurum8 <dineshgurumoorthy8@gmail.com>
2024-09-02 18:14:14 -0500

    Update utils_dinesh.py

--- utils_dinesh.py
+++ utils_dinesh.py
@@ -1,51 +1,70 @@
-''' ITERATION 1
+''' Final Example
 
-Module: Stellar Analytics - Reusable Module for My Data Analytics Projects
+Module: Mahitha Analytics - Reusable Module for My Data Analytics Projects
 
-This module provides a simple, reusable foundation for my analytics projects. 
-When we work hard to write useful code, we want it to be reusable.
-A good byline could be used in every Python analytics project we do.
-
-Process:
-
-We don't write code from top to bottom; instead, we often write it from the outside in.
-Here's what a first draft of my utils_case.py might look like:
-
-1. I start with this docstring at the very beginning.
-   I use it to clarify the purpose of my Python file and organize my thoughts.
-   
-2. I'll declare a global variable for my byline string and just set it to some simple text.
-
-3. I'll declare a main() function for my module. When I run this script, I can use main() to test my byline.
-
-4. I'll add the boilerplate conditional execution code so I only run the main() function when 
-   this script is executed directly (but not when I import it into another file).
-
-I'll test it in an online interpreter to ensure this version runs correctly before continuing.
 '''
 
+import statistics
+
+
+has_international_clients: bool = True
+years_in_operation: int = 10
+average_client_satisfaction: float = 4.7 
+skills_offered: list = ["Data Analysis", "Machine Learning", "Business Intelligence"]
+client_satisfaction_scores: list = [4.8, 4.6, 4.9, 5.0, 4.7]
+temperature_values: list = [40.8, 37.6, 44.9, 25.0, 44.7]
+
 #####################################
-# Declare a global variable named byline.
+# Calculate Basic Statistics using built in functions.
+
 #####################################
 
-byline: str = 'Stellar Analytics: Delivering Professional Insights'
+# Calculate basic stats using built-in functions min(), max(), and statistics module functions mean() and stdev().
+min_score:  float = min(client_satisfaction_scores)
+max_score:  float = max(client_satisfaction_scores)
+mean_score:  float = statistics.mean(client_satisfaction_scores)
+stdev_score:  float = statistics.stdev(client_satisfaction_scores)
+min_temp:  float = min(temperature_values)
+max_temp:  float = max(temperature_values)
+mean_temp:  float = statistics.mean(temperature_values)
+stdev_temp:  float = statistics.stdev(temperature_values)
+
+byline: str = f"""
+-------------------------------------------------------------
+Mahitha Analytics : Delivering Professional Insights
+-------------------------------------------------------------
+Has International Clients:  {has_international_clients}
+Years in Operation:  {years_in_operation}
+Skills Offered:  {skills_offered}
+Client Satisfaction Scores:  {client_satisfaction_scores}
+Minimum Client Satisfacton Score:  {min_score}
+Maximum Client Satisfaction Score:  {max_score}
+Average Client Satisfaction Score:  {mean_score}
+Standard Deviation of Client Satisfaction Scores:  {stdev_score:.2f}
+Minimum Temperature (F):  {min_temp}
+Maximum Temperature (F):  {max_temp}
+Mean Temperature (F):  {mean_temp}
+Standard Deviation of Temperature (F):  {stdev_temp}
+
+"""
+
+#####################################
+# Define the get_byline() function.
+#####################################
+
+def get_byline() -> str:
+    '''Return a byline for my analytics projects.'''
+    return byline
 
 #####################################
 # Define a main() function for this module.
 #####################################
 
-# Create a function named main.
-# A function is a block of code that performs a specific task.
-# This function will simply print the byline to the console.
-# Add a type hint to indicate that this function doesn't return anything when called 
-# (that is, it has a Python type of None).
-# It doesn't need any additional information passed in, 
-# so there's nothing needed inside the parentheses.
-# Everything afer the colon (:) must be indented (usually 4 spaces)
+# The main() function now calls the get_byline function to retrieve the byline.
 
 def main() -> None:
-    '''Print the byline to the console when this function is called.'''
-    print(byline)
+    '''Print results of get_byline() when main() is called.'''
+    print(get_byline())
 
 #####################################
 # Conditional Execution - Only call main() when executing this module as a script.
