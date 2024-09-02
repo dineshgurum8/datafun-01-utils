8791a235b08894acc7325e6b5e91e47368983389
dineshgurum8 <dineshgurumoorthy8@gmail.com>
2024-09-02 18:11:47 -0500

    Create utils_dinesh_2.py

---
+++ utils_dinesh_2.py
@@ -0,0 +1,26 @@
+'''ITERATION 2'''
+
+####################
+## Define the get_byline () Function
+###############
+def get_byline()-> str:
+    "'Return a byline for my analytics projects.'"
+    retutn byline
+
+
+#############
+## Define a main() function for this module.
+##############
+
+### The main function now calls get_byline() to retrieve the byline.
+
+def main() -> None:
+    "'Print the byline to the console when this function is called.'"
+    print(get_byline)
+
+##########
+##Conditional Execution##
+#############
+
+if __name__ == '__main__':
+    main()
